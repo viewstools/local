8a76a44924b5e11c0d1969604d9643e2b2fe1767
Cormac Neeson <cormac.neeson@hmhco.com>
2018-03-26 12:57:54 +0100

    fix: integrate PR feedback

--- get-i18n.js
+++ get-i18n.js
@@ -1,24 +1,21 @@
+const locales = require('i18n-locales')
+
+const LOCAL_SCOPES = locales.map(item => item.replace(/-/g, ''))
 const isText = line => /^(text|placeholder) /.test(line)
-
 const withoutSlot = line => line.replace(/^(\<.+?)\s/, '')
-
 const isBlock = line => /^[A-Z]/.test(line)
 const getBlock = line => line.split(' ')[0]
 const getText = line => line.replace(/^(text|placeholder) /, '')
 
-const locales = require('i18n-locales')
-const LOCAL_SCOPES = locales.map(item => item.replace(/-/g, ''))
-
-function getI18n(view, rtext) {
+function getI18n({view, rtext, defaultLanguage = 'en'}) {
   const text = rtext.replace(/\r\n/g, '\n')
   const lines = text.split('\n').map(line => line.trim())
 
   const obj = {};
-  const defaultLanguage = {};
+  const base = {};
   let currentBlock = null
   let currentView = null
   const regexList = LOCAL_SCOPES.map(shortcode => new RegExp(`^when <${shortcode}`) );
-  let isTranslation = null;
   let textKey;
 
   lines.forEach((line, index) => {
@@ -27,10 +24,10 @@ function getI18n(view, rtext) {
     }
     if (isText(line)) {
       const text = getText(line)
-      isTranslation = regexList.some(regex => regex.test(lines[index-1]));
+      const isTranslation = regexList.some(regex => regex.test(lines[index-1]));
       if(!isTranslation){
         textKey = line;
-        defaultLanguage[`${view}/${currentBlock}/${textKey}`] = withoutSlot(text)
+        base[`${view}/${currentBlock}/${textKey}`] = withoutSlot(text)
       } else {
         const langShortCode = lines[index-1].split('<')[1];
         if(!obj.hasOwnProperty(langShortCode)){
@@ -39,7 +36,7 @@ function getI18n(view, rtext) {
         obj[langShortCode][`${view}/${currentBlock}/${textKey}`] = withoutSlot(text)
       }
     }
-    obj[`default`] = defaultLanguage;
+    obj[defaultLanguage] = base;
   })
 
   return obj
